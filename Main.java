b1f8e0f3721263749088f1ef441c2778e335f93f
welkerbrendon <wel15018@byui.edu>
2017-01-25 13:29:38 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	    System.out.println("Hello World");
 	    System.out.println("Still practicing.");
+	    System.out.println("You guessed it, still practicing");
     }
 }
